eb7e95cf5723e042e29e743d53cd2d4e43eaaea2
Jade Ye <w0430394@nscc.ca>
2022-08-04 12:05:02 -0300

    Implement scroll menu and its catalogue cards.

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,21 +1,40 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import BodyParts from './BodyParts'
 import { Box } from '@mui/material'
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu'
+import { MdNavigateBefore, MdNavigateNext } from 'react-icons/md'
+
+
+const LeftArrow = () => {
+  const { scrollPrev } = React.useContext(VisibilityContext);
+
+  return (
+    <MdNavigateBefore onClick={() => scrollPrev()} className="left-arrow" size='48px' />
+  );
+};
+
+const RightArrow = () => {
+  const { scrollNext } = React.useContext(VisibilityContext)
+
+  return (
+    <MdNavigateNext onClick={() => scrollNext()} className="right-arrow" size='48px' />
+  );
+};
 
 const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
   return (
-    <div>
+    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>        
       { data.map((item) => (
         <Box
           key={item.id || item}
-          itemId={item.id}
+          itemId={item.id || item}
           title={item.id || item}
           m='0 40px'
         >
           <BodyParts item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
         </Box>
-      ) ) }
-    </div>
+      ) ) }        
+    </ScrollMenu>    
   )
 }
 
