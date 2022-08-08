6d2c3947e3316b2e3de3dd983beec788568b57e6
Jade Ye <71286765+JadeYe2020@users.noreply.github.com>
2022-08-07 22:55:57 -0300

    Captalize some text (#1)

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -15,10 +15,10 @@ const HeroBanner = ({ bodyParts, setExercisesToShow, bodyPart, setBodyPart, allE
         <Typography color='#1A535C' fontWeight='600' fontSize='26px'>
           Find any exercise
         </Typography>
-        <Typography color='#1A535C' fontWeight='700' mb='23px' mt='30px' sx={{
-          fontSize: { lg: '44px', xs: '40px'}
+        <Typography color='#1A535C' fontWeight='700' mb='23px' mt='30px'
+          sx={{ fontSize: { lg: '44px', xs: '40px'}
         }}>
-          that works for you!
+          That Works For You!
         </Typography>
         <Typography color='#1A535C' fontSize='22px' lineHeight='35px' mb={4}>
           Check out hundreds of awesome exercises here
